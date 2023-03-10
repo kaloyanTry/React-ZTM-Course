a37487b7f4fc829228302fa83fa954035f0f808c
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2023-03-10 21:02:18 +0200

    App First Stage
    Adding filter and incudes functionality to search in input field

---
+++ FirstSteps/AppFirstStage.js
@@ -0,0 +1,64 @@
+import './App.css';
+
+import { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: '',
+    };
+    console.log('constructor');
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
+  render() {
+    console.log('render');
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField);
+    });
+    return (
+      <div className='App'>
+        <input
+          className='searchBox'
+          type='search'
+          placeholder='search monsters'
+          onChange={(event) => {
+            // console.log(event.target.value);
+            const searchField = event.target.value.toLowerCase();
+
+            this.setState(() => {
+              return { searchField };
+            });
+          }}
+        />
+        {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
