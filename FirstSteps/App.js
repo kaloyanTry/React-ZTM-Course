9e1d324305e4e220bb41d3ca193fb5236f0d6531
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2023-03-14 16:28:10 +0200

    Class App variant
    Class React Component

--- FirstSteps/App.js
+++ FirstSteps/App.js
@@ -1,71 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
 
 import { Component } from 'react';
+
+import SearchBox from './components/search-box/search-box.component';
+import CardList from './components/card-list/card-list.component';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      names: [],
+      monsters: [],
       searchField: '',
     };
   }
 
   componentDidMount() {
+    // console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) =>
         this.setState(
           () => {
-            return { names: users };
-          },
-          () => {
-            console.log(this.state);
+            return { monsters: users };
           }
+          // () => {
+          //   console.log(this.state);
+          // }
         )
       );
   }
 
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLowerCase();
+
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
-    const filteredNames = this.state.names.filter((name) => {
-      return name.name.toLowerCase().includes(this.state.searchField);
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
     });
 
     return (
       <div className='App'>
-        <header className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
-        </header>
-        <input
-          className='searchBox'
-          type='search'
-          placeholder='search name'
-          onChange={(event) => {
-            const searchField = event.target.value.toLowerCase();
-            this.setState(() => {
-              return { searchField };
-            });
-          }}
+        <h1 className='app-title'>Monster Rolodex</h1>
+
+        <SearchBox
+          className='monsters-search-box'
+          onChangeHandler={onSearchChange}
+          placeholder='search monsters'
         />
-        {filteredNames.map((name) => {
-          return (
-            <div key={name.id}>
-              <h1>{name.name}</h1>
-            </div>
-          );
-        })}
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
