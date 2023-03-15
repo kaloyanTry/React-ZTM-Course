81991e8898a2323cc0aaccef65cbfaaa7e0d3222
Kaloyan Ganchev <kaloyantryavna@gmail.com>
2023-03-15 21:43:50 +0200

    Add files via upload

--- Monsters-Rolodex-App/components/card/card.component.jsx
+++ Monsters-Rolodex-App/components/card/card.component.jsx
@@ -1,21 +1,18 @@
-import { Component } from 'react';
 import './card.style.css';
 
-class Card extends Component {
-  render() {
-    const { name, email, id } = this.props.monster;
+const Card = ({ monster }) => {
+  const { name, email, id } = monster;
 
-    return (
-      <div className='card-container' key={id}>
-        <img
-          src={`https://robohash.org/${id}?set=set2$size=180x180`}
-          alt={`monster${name}`}
-        />
-        <h2>{name}</h2>
-        <p>{email}</p>
-      </div>
-    );
-  }
-}
+  return (
+    <div className='card-container' key={id}>
+      <img
+        src={`https://robohash.org/${id}?set=set2$size=180x180`}
+        alt={`monster${name}`}
+      />
+      <h2>{name}</h2>
+      <p>{email}</p>
+    </div>
+  );
+};
 
 export default Card;
